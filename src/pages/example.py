aecf2ef84dd34a0dc85b03c19ee31262480cf69c
Logan Kirkland <logan@logankirk.land>
2024-05-05 23:07:09 -0400

    Removed unneeded template files

--- src/pages/example.py
+++
@@ -1,8 +0,0 @@
-from src.pages.page import Page
-
-
-class ExamplePage(Page):
-    def __init__(self, driver, test_parameters):
-        super().__init__(driver, test_parameters)
-
-        self.locators.update(self.all_locators["example"])
