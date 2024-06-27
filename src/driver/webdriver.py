fc79f3ff7bcd817d33b16d9abb511a9dd1b225a7
Logan Kirkland <logan@logankirk.land>
2024-06-27 02:15:55 -0400

    Print versions to console

--- src/driver/webdriver.py
+++ src/driver/webdriver.py
@@ -6,3 +6,9 @@ options = ChromeOptions()
 class ChromeDriver(Chrome):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        print("Browser version:", self.capabilities["browserVersion"])
+        print(
+            "Driver version:",
+            self.capabilities["chrome"]["chromedriverVersion"].split(" ")[0],
+        )
+
